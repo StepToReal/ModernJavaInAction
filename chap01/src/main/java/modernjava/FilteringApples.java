805e71740131bf0e5c15f53ed80bd027805b738d
steptoreal <jcob0920@gmail.com>
2022-11-11 00:01:35 +0900

    람다, 스트림 예제 소스 추가

--- chap01/src/main/java/modernjava/FilteringApples.java
+++ chap01/src/main/java/modernjava/FilteringApples.java
@@ -1,7 +1,9 @@
 package modernjava;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FilteringApples {
     public static void main(String[] args) {
@@ -14,8 +16,27 @@ public class FilteringApples {
 
         System.out.println(filterGreenApples(inventory));
         System.out.println(filterHeavyApples(inventory));
+        System.out.println();
+
         System.out.println(filterApples(inventory, FilteringApples::isGreenApple));
         System.out.println(filterApples(inventory, FilteringApples::isHeavyApple));
+        System.out.println();
+
+        System.out.println(filterApples(inventory, (Apple a) -> "GREEN".equals(a.getColor())));
+        System.out.println(filterApples(inventory, (Apple a) -> a.getWeight() > 150));
+        System.out.println(filterApples(inventory, (Apple a) -> a.getWeight() < 80 || "RED".equals(a.getColor())));
+        System.out.println();
+
+        List<Apple> heavyApples = inventory.stream().filter((Apple a) -> a.getWeight() > 150).collect(Collectors.toList());
+        System.out.println(heavyApples);
+
+        List<Apple> parallelHeavyApples = inventory.parallelStream()
+                .filter((Apple a) -> a.getWeight() > 150)
+                .collect(Collectors.toList());
+        System.out.println(parallelHeavyApples);
+
+//        Collections.sort();
+//        heavyApples.sort();
     }
 
     static List<Apple> filterApples(List<Apple> inventory, MyPredicate<Apple> p) {
