a131cb69dbcc6b3c69817d9504703b64fbe1906b
steptoreal <jcob0920@gmail.com>
2023-03-08 23:48:50 +0900

    chap07 병렬스트림 성능 측정
    - jmh 의존성 추가
    - 병렬 스트림 benchmark 측정 코드 구현

---
+++ chap07_parallel/src/jmh/java/modernjava/Dish.java
@@ -0,0 +1,22 @@
+package modernjava;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Dish {
+    private final String name;
+    private final boolean vegetarian;
+    private final int calories;
+    private final Type type;
+
+    public enum Type { MEAT, FISH, OTHER }
+
+    @Override
+    public String toString() {
+        return String.format("(%s, %d, %s)", name, calories, type.toString());
+    }
+}
